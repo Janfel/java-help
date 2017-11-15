73661111ffd074f87a55720a4978a6641a0811ab
Janfel <33464477+Janfel@users.noreply.github.com>
2017-11-15 19:41:04 +0100

    New Methods
    Added three example methods to Snippets/Method.java

--- Snippets/Method.java
+++ Snippets/Method.java
@@ -1,4 +1,3 @@
-
 void helloWorld() {
 	/* This Method has the return type "void",
 	which means it is not returning something.
@@ -6,3 +5,27 @@ void helloWorld() {
 	System.out.println("Hello World");
 	/* Here we just print "Hello World" to the console. */
 } /* The method body ends without return statement */
+
+int getOne() {
+	/* This method returns a variable of the type int.
+	Other than that nothing differs from the first example. */
+	return 1;
+	/* The return statement returns the integer 1.
+	Without return statement this method would not compile. */
+}
+
+int addTogether(int a, int b) {
+	/* This method adds the integers a and b, which get passed as parameters. */
+	return a + b;
+}
+
+String concat(String... strings) {
+	/* Here we can pass as many Strings as we need. */
+	String string = "";
+	/* An empty String */
+	for (String s : strings) {
+		string += s;
+	}
+	/* We append every String in strings to our empty String. */
+	return string;
+}
