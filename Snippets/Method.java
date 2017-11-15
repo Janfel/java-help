7b55ca69d288ec8c0d90b08131f69b70ef643032
Janfel <33464477+Janfel@users.noreply.github.com>
2017-11-15 19:20:45 +0100

    Revert "Revert "Added Snippets.""
    This reverts commit be3d8434d0c6a0f3a6b02b2abbe8f109b8236879.

---
+++ Snippets/Method.java
@@ -0,0 +1,8 @@
+
+void helloWorld() {
+	/* This Method has the return type "void",
+	which means it is not returning something.
+	It is only visible by classes in the same package. */
+	System.out.println("Hello World");
+	/* Here we just print "Hello World" to the console. */
+} /* The method body ends without return statement */
